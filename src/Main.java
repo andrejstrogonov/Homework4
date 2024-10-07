0ea9e50346e24d9e0c03faaab83aea74f6c6e0e4
andrejstrogonov <strogonovandrej@hotmail.com>
2024-10-07 12:42:56 +0300

    task1 complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int peopleAge = 19;
+        if (peopleAge >= 18) {
+            System.out.println("Если возраст человек равен "+peopleAge+" он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человек равен "+peopleAge+"  он не достиг совершеннолетия, нужно немного подождать");
+        }
+        int childAge = 5;
+        if (childAge >= 18) {
+            System.out.println("Если возраст человек равен "+childAge+" он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человек равен "+childAge+"  он не достиг совершеннолетия, нужно немного подождать");
+        }
+
     }
 }
