440bf4f8b9a578095d3650f857f44bfe1786ba58
andrejstrogonov <strogonovandrej@hotmail.com>
2024-10-07 13:38:19 +0300

    task2 complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int firstTemperature = -10;
+        int secondTemperature = 10;
+        if (firstTemperature >=5){
+            System.out.println("На улице "+secondTemperature+" можно идти без шапки");
+        }else {
+            System.out.println("На улице "+firstTemperature+" нужно надеть шапку");
+        }
+        if (secondTemperature >=5){
+            System.out.println("На улице "+secondTemperature+" можно идти без шапки");
+        }else {
+            System.out.println("На улице "+firstTemperature+" нужно надеть шапку");
+        }
     }
 }
