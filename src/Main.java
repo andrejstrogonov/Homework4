9a1c42a214e42a40c51f0d17848537e8e4b166bb
andrejstrogonov <strogonovandrej@hotmail.com>
2024-10-07 13:55:18 +0300

    task3 complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int firstSpeed = 120;
+       int secondSpeed = 50;
+       int speedThrashhold = 60;
+       if (firstSpeed>=speedThrashhold){
+           System.out.println("Если скорость "+firstSpeed+" ,то придется заплатить штраф");
+       }else {
+           System.out.println("Если скорость "+secondSpeed+" ,то можно ездить спокойно");
+       }
+        if (secondSpeed>=speedThrashhold){
+            System.out.println("Если скорость "+firstSpeed+" ,то придется заплатить штраф");
+        }else {
+            System.out.println("Если скорость "+secondSpeed+" ,то можно ездить спокойно");
+        }
     }
 }
