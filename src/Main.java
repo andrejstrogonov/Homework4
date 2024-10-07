1107fb690e1269fc95cd72ec3f32d8d861903d92
andrejstrogonov <strogonovandrej@hotmail.com>
2024-10-07 13:39:21 +0300

    Fix master branch

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int firstTemperature = -10;
-        int secondTemperature = 10;
-        if (firstTemperature >=5){
-            System.out.println("На улице "+secondTemperature+" можно идти без шапки");
-        }else {
-            System.out.println("На улице "+firstTemperature+" нужно надеть шапку");
-        }
-        if (secondTemperature >=5){
-            System.out.println("На улице "+secondTemperature+" можно идти без шапки");
-        }else {
-            System.out.println("На улице "+firstTemperature+" нужно надеть шапку");
-        }
+        System.out.println("Hello world!");
     }
 }
