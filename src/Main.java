604b80fbb250cc8f5a89e8ce29369b9b0e058aa4
andrejstrogonov <strogonovandrej@hotmail.com>
2024-10-07 15:54:55 +0300

    task7 complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int min = 10;
+        int max = 100;
+        int one = rnd(min,max);
+        int two = rnd(min,max);
+        int three = rnd(min,max);
+        if(one>=two&&one>=three){
+            System.out.println("Большее число one: " +one);
+        }
+        if(two>=one&&two>=three){
+            System.out.println("Большее число two: " +two);
+        }
+        if(three>=two&&three>=one){
+            System.out.println("Большее число three: " +three);
+        }
+
+    }
+    public static int rnd(int min,int max){
+        max = -min;
+        return (int) (Math.random() * ++max) + min;
     }
 }
